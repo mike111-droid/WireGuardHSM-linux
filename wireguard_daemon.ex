a19dbeaea693790162319b55547c815243546feb
mike111-droid <74792224+mike111-droid@users.noreply.github.com>
2022-02-15 15:23:30 +0100

    Add files via upload

---
+++ wireguard_daemon.ex
@@ -0,0 +1,4 @@
+spawn ./scsh3
+expect ">"
+send "load(\"wireguard_daemon.js\");\n"
+expect ">"
---
+++ wireguard_daemon.js
@@ -0,0 +1,32 @@
+/*
+ * Example to decrypt using RSA with PKCS#1 formats V1.5 and OAEP
+ */
+
+
+SmartCardHSM = require('scsh/sc-hsm/SmartCardHSM').SmartCardHSM;
+HSMKeyStore = require('scsh/sc-hsm/HSMKeyStore').HSMKeyStore;
+PKCS1 = require('scsh/pkcs/PKCS1').PKCS1;
+
+// Create nessecary vars
+var crypto = new Crypto();
+var card = new Card(_scsh3.reader);
+var sc = new SmartCardHSM(card);
+sc.verifyUserPIN(new ByteString("123456", ASCII));
+var ks = new HSMKeyStore(sc);
+
+// Get Key
+var key = ks.sc.getKey("RSA_KEY");
+
+// Digest old psk
+var oldpsk = "2022.02.15.13";
+var message = new ByteString(oldpsk, ASCII);
+var hash = crypto.digest(Crypto.SHA_256, message);
+print(hash);
+
+// Sign hash old psk and than hash again
+print(key);
+print(key.getType());
+print("Crypto.RSA_PKCS1");
+var signature = sc.getCrypto().sign(key, Crypto.RSA_SHA256, message);
+var result = crypto.digest(Crypto.SHA_256, signature);
+print("++++ " + result.toBase64().toString(ASCII));

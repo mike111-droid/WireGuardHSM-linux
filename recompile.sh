f050edbc8ab59dc4c8cb60adfb1cd758cb76a492
mike111-droid <74792224+mike111-droid@users.noreply.github.com>
2022-02-17 11:53:58 +0100

    Update recompile.sh

--- recompile.sh
+++ recompile.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-gcc -o start wireguardhsm/wireguardhsm.c
+INTERFACE=$(cat wireguardhsm/settings.h | grep "#define INTERFACE" | grep -oP '"\K[^"]+')
+ENABLE_HSM=$(cat wireguardhsm/settings.h | grep "#define ENABLE_HSM" | grep -oP '"\K[^"]+')
+ENABLE_TIMESTAMP=$(cat wireguardhsm/settings.h | grep "#define ENABLE_TIMESTAMP" | grep -oP '"\K[^"]+')
+
+gcc -o start_$INTERFACE-hsm_$ENABLE_HSM-timestamp_$ENABLE_TIMESTAMP wireguardhsm/wireguardhsm.c
